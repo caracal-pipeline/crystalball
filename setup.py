7b278396976acfefb68b0aedc5fae7318ad46e97
Simon Perkins <simon.perkins@gmail.com>
2019-06-20 16:46:34 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -4,31 +4,30 @@ import os
 from distutils.core import setup
 
 requirements = [
-"codex-africanus[complete]>=0.1.4",
-"xarray-ms",
-"regions"
+    "codex-africanus[complete] >= 0.1.8",
+    "xarray-ms >= 0.1.8",
+    "regions"
 ]
 
 PACKAGE_NAME = 'crystalball'
 __version__ = '0.1.3'
 
-setup(name = PACKAGE_NAME,
-    version = __version__,
-    description = "Predicts visibilities from a parameterised sky model",
-    author = "Paolo Serra",
-    author_email = "paolo80serra@gmail.com",
-    url = "https://github.com/paoloserra/crystalball",
-    packages=["Crystalball"], 
-    install_requires = requirements,
-    include_package_data = True,
-    scripts = ["bin/" + i for i in os.listdir("bin")],
-    license=["GNU GPL v2"],
-    classifiers=[
+setup(name=PACKAGE_NAME,
+      version=__version__,
+      description="Predicts visibilities from a parameterised sky model",
+      author="Paolo Serra",
+      author_email="paolo80serra@gmail.com",
+      url="https://github.com/paoloserra/crystalball",
+      packages=["Crystalball"],
+      install_requires=requirements,
+      include_package_data=True,
+      scripts=["bin/" + i for i in os.listdir("bin")],
+      license=["GNU GPL v2"],
+      classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Astronomy"
-    ]
-     )
+      ])
