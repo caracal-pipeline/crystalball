e4216e42d6c605f317673eff2638c2f3c4630978
Simon Perkins <simon.perkins@gmail.com>
2020-04-01 15:03:22 +0200

    update budgeting code.
    Previously, the predict memory usage was V x S where
    V and S were the number of visibilities and sources
    in each chunk, respectively.
    
    This has been changed to V + S.

--- crystalball/budget.py
+++ crystalball/budget.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_budget(nr_sources, nr_rows, nr_chans, nr_corrs, data_type, cb_args,
-               fudge_factor=2., row2source_ratio=100):
+               fudge_factor=0.55, row2source_ratio=100):
     systmem = np.float(psutil.virtual_memory()[0])
     if not cb_args.num_workers:
         nrthreads = psutil.cpu_count()
@@ -20,38 +20,32 @@ def get_budget(nr_sources, nr_rows, nr_chans, nr_corrs, data_type, cb_args,
 
     data_type = {'complex': 'complex64', 'dcomplex': 'complex128'}[data_type]
     data_bytes = np.dtype(data_type).itemsize
-    bytes_per_row_source = nr_chans*nr_corrs*data_bytes
-    memory_per_row_source = bytes_per_row_source * fudge_factor
+    bytes_per_row = nr_chans*nr_corrs*data_bytes
+    memory_per_row = bytes_per_row * fudge_factor
 
     if cb_args.model_chunks and cb_args.row_chunks:
         rows_per_chunk = cb_args.row_chunks
         sources_per_chunk = cb_args.model_chunks
-        print('sources per chunk = {0:.0f} (user setting)'.format(
-            sources_per_chunk))
-        print('rows per chunk    = {0:.0f} (user setting)'.format(
-            rows_per_chunk))
-        memory_usage = (rows_per_chunk * sources_per_chunk *
-                        memory_per_row_source * nrthreads)
-        print('expected memory usage = {0:.2f} GB'.format(
-            memory_usage/1024**3))
+        strat_type = "(user settings)"
     elif not cb_args.model_chunks and not cb_args.row_chunks:
-        allowed_rowXsource_per_thread = systmem * \
-            cb_args.memory_fraction / memory_per_row_source / nrthreads
-        rows_per_chunk = np.int(np.minimum(nr_rows, np.sqrt(
-            allowed_rowXsource_per_thread * row2source_ratio)))
-        sources_per_chunk = np.int(np.minimum(
-            nr_sources, rows_per_chunk / row2source_ratio))
-        print('sources per chunk = {0:.0f} (auto setting)'.format(
-            sources_per_chunk))
-        print('rows per chunk    = {0:.0f} (auto setting)'.format(
-            rows_per_chunk))
-        memory_usage = (rows_per_chunk * sources_per_chunk *
-                        memory_per_row_source*nrthreads)
-        print('expected memory usage = {0:.2f} GB'.format(
-            memory_usage/1024**3))
+        allowed_rows_per_thread = (systmem * cb_args.memory_fraction /
+                                   (memory_per_row * nrthreads))
+        rows_per_chunk = int(min(nr_rows, allowed_rows_per_thread))
+        sources_per_chunk = int(min(nr_sources,
+                                    rows_per_chunk / row2source_ratio))
+        strat_type = "(auto settings)"
     else:
         raise ValueError('For now you must set both row and source chunk, or '
                          'leave both unset (=0); '
                          'you cannot set only one of them.')
+
+    print('sources per chunk = {0:.0f} {1}'
+          .format(sources_per_chunk, strat_type))
+    print('rows per chunk    = {0:.0f} {1}'
+          .format(rows_per_chunk, strat_type))
+
+    memory_usage = (rows_per_chunk * memory_per_row * nrthreads
+                    + sources_per_chunk)
+    print('expected memory usage = {0:.2f} GB'.format(memory_usage/1024**3))
 
     return rows_per_chunk, sources_per_chunk
