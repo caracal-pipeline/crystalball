1e2999fa457dc448c55bc45c552b5291c2d25d15
Simon Perkins <simon.perkins@gmail.com>
2020-04-09 09:16:37 +0200

    Depend on codex-africanus==0.2.2

--- setup.py
+++ setup.py
@@ -3,13 +3,7 @@
 from distutils.core import setup
 
 requirements = [
-    # TODO(sjperkins)
-    # Update this once we're happy with the branch
-    # "codex-africanus[dask] >= 0.2.1",
-    "codex-africanus[dask]"
-    "@git+https://github.com/ska-sa/codex-africanus.git"
-    "@master",
-
+    "codex-africanus[dask] >= 0.2.2",
     "dask-ms >= 0.2.3",
     "loguru",
     "regions",
